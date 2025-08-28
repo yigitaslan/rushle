398c067e52cd1a47461811b4b60dea6277d0256a
yigitaslan <ygtaslan24@hotmail.com>
2025-08-28 19:25:47 +0300

    Pages changes 7

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withHashLocation } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes, withHashLocation()),
-  ],
-}).catch(err => console.error(err));
+  providers: [provideZonelessChangeDetection()],
+}).catch(console.error);
